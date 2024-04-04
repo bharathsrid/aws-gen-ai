9815f56050b6f2315b02b98b2221120120d05bf9
Bharath Sridharan <bharsrid@amazon.com>
2024-04-04 12:52:18 +0000

    adding get stock change lambda building blocks. not fully functional yet.

---
+++ bedrock-agent-ta/get-stock-change/.aws-sam/cache/8253c5d9-7cb6-454c-9aec-6743015c3181/app.py
@@ -0,0 +1,89 @@
+import json
+import pandas as pd
+import boto3
+
+# import requests
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+
+    print(event)
+    responses = []
+    api_path = event['apiPath']
+    params = dict((param['name'], param['value']) for param in event['parameters'])
+    stock_list = params['stockList']
+
+    # download csv file from a s3 bucket into /tmp
+    s3 = boto3.resource('s3')
+    s3.Bucket('bharsrid-bedrock-agent-yf-demo').download_file('stock_hist/stock_data_1y.csv', '/tmp/stock_data_1y.csv')
+    stock_1y_df = pd.read_csv('/tmp/stock_data_1y.csv')
+    # convert the Date column in dataframe from string to datetime date
+    stock_1y_df['Date'] = pd.to_datetime(stock_1y_df['Date'])
+
+
+    if api_path=="/get-stock-change":
+        print("in stock change")
+
+        for stock in stock_list:
+            stock_df = stock_1y_df[stock_1y_df['Symbol'] == stock]
+        # filter stock df to retain only last 1 week data using the Date column as reference
+        stock_1w_df = stock_df[stock_df['Date'] > stock_df['Date'].max() - pd.Timedelta(days=7)]
+        stock_1w_df['Date'] = pd.to_datetime(stock_1w_df['Date'], infer_datetime_format=True)
+        print(stock_1w_df['Date'].dtype)
+        # reconvert datetime of Date column to string in dd-MMM-YYYY format
+        stock_1w_df['Date'] = stock_1w_df['Date'].dt.strftime('%d-%b-%Y')
+        body = stock_1w_df.to_json(orient='records')
+
+    else:
+        body = {"{} is not a valid api, try another one.".format(api_path)}
+
+
+    response_body = {
+        'application/json': {
+            'body': json.dumps(body)
+        }
+    }
+
+    action_response = {
+        'actionGroup': event['actionGroup'],
+        'apiPath': event['apiPath'],
+        'httpMethod': event['httpMethod'],
+        'httpStatusCode': 200,
+        'responseBody': response_body
+    }
+
+    responses.append(action_response)
+
+    api_response = {
+        'messageVersion': '1.0',
+        'response': action_response}
+
+    return api_response
