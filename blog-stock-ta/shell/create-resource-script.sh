0c34b9d711ab9123c1ea9aaf72b627c1a057546d
Bharath Sridharan <bharsrid@amazon.com>
2024-06-05 13:21:16 +0000

    working shell script and yahoo finance

--- blog-stock-ta/shell/create-resource-script.sh
+++ blog-stock-ta/shell/create-resource-script.sh
@@ -1,6 +1,32 @@
-export ARTIFACT_BUCKET_NAME="$STACK_NAME-ta-agent-resources-$RANDOM"
+stack_name=""
+aws_region=""
 
-aws s3 mb s3://${ARTIFACT_BUCKET_NAME} --region ${AWS_REGION} --profile child
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --stack-name)
+            stack_name="$2"
+            shift 2
+            ;;
+        --aws-region)
+            aws_region="$2"
+            shift 2
+            ;;
+        *)
+            echo "Invalid option: $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+# Check if both parameters are provided
+if [ -z "${stack_name}" ] || [ -z "${aws_region}" ]; then
+    echo "Error: --stack-name and --aws-region are required." >&2
+    exit 1
+fi
+
+export ARTIFACT_BUCKET_NAME="$stack_name-ta-agent-resources-$RANDOM"
+
+aws s3 mb s3://${ARTIFACT_BUCKET_NAME} --region ${aws_region} --profile child
 echo "Successfully created bucket $ARTIFACT_BUCKET_NAME"
 aws s3 cp ../lambda/ s3://${ARTIFACT_BUCKET_NAME}/lambda/ --recursive --include "*.zip" --profile child
 
@@ -8,9 +34,18 @@ aws s3 cp ../lambda/ s3://${ARTIFACT_BUCKET_NAME}/lambda/ --recursive --include 
 
 aws cloudformation create-stack \
 --profile child \
---stack-name ${STACK_NAME} \
+--stack-name ${stack_name} \
 --template-body file://../cfn/yfn-daily-fetch.yaml \
 --parameters \
 ParameterKey=S3Bucket,ParameterValue=${ARTIFACT_BUCKET_NAME} \
 --capabilities CAPABILITY_NAMED_IAM \
---region ${AWS_REGION}
+--region ${aws_region}
+
+# sleep 5
+aws cloudformation describe-stacks --stack-name $stack_name --region ${aws_region} --query "Stacks[0].StackStatus" --profile child
+aws cloudformation wait stack-create-complete --stack-name $stack_name --region ${aws_region} --profile child
+aws cloudformation describe-stacks --stack-name $stack_name --region ${aws_region} --query "Stacks[0].StackStatus" --profile child
+
+echo "Successfully created stack $stack_name"
+echo "Successfully created bucket $ARTIFACT_BUCKET_NAME"
+echo "These details would be needed while clean up"
--- blog-stock-ta/shell/delete-resource-script.sh
+++ blog-stock-ta/shell/delete-resource-script.sh
@@ -1,11 +1,42 @@
-aws --profile child s3 rb s3://blogstack-3-ta-agent-resources-11623
-aws --profile child s3 rb s3://blogstack-3-ta-agent-resources-19148
-aws --profile child s3 rb s3://blogstack-4-ta-agent-resources-8451
-aws --profile child s3 rb s3://blogstack-5-ta-agent-resources-12939
-aws --profile child s3 rb s3://blogstack-6-ta-agent-resources-10613
-aws --profile child s3 rb s3://blogstack-6-ta-agent-resources-11184
-aws --profile child s3 rb s3://blogstack-6-ta-agent-resources-209
-aws --profile child s3 rb s3://blogstack-6-ta-agent-resources-25665
-aws --profile child s3 rb s3://blogstack-6-ta-agent-resources-3256
-aws --profile child s3 rb s3://blogstack-6-ta-agent-resources-3677
-aws --profile child s3 rb s3://blogstack-6-ta-agent-resources-6049
+# Parse options
+bucket_name=""
+stack_name=""
+aws_region=""
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --bucket-name-to-delete)
+            bucket_name="$2"
+            shift 2
+            ;;
+        --stack-name)
+            stack_name="$2"
+            shift 2
+            ;;
+        --aws-region)
+            aws_region="$2"
+            shift 2
+            ;;
+        *)
+            echo "Invalid option: $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+# Check if both parameters are provided
+if [ -z "${bucket_name}" ] || [ -z "${stack_name}" ] || [ -z "${aws_region}" ]; then
+    echo "Error: --bucket-name-to-delete, --stack-name and --aws-region are required." >&2
+    exit 1
+fi
+
+aws s3 rm s3://${bucket_name} --region ${aws_region} --recursive --profile child
+aws s3 rb s3://${bucket_name} --region ${aws_region} --profile child
+
+echo "Deleting CloudFormation Stack: $stack_name"
+
+aws cloudformation delete-stack --stack-name $stack_name --region ${aws_region} --profile child
+aws cloudformation describe-stacks --stack-name $stack_name --region ${aws_region} --query "Stacks[0].StackStatus" --profile child
+
+aws cloudformation wait stack-delete-complete --stack-name $stack_name --region ${aws_region} --profile child
+echo "DELETE_COMPLETE"
